9f999f38c358607d54940308e6620978017d5063
Jayce77 <xjaycex@hotmail.com>
2019-01-25 21:55:28 -0500

    Added Link component for navigation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
     <p>Hello Next.js</p>
-    <a href="/"> Home </a>
-    <a href="/about"> About </a>
-    <a href="/blogs"> Blog </a>
-    <a href="/cv"> CV </a>
-    <a href="/portfolios"> Portfolios </a>
+    <Link href="/">
+      <a> Home </a>
+    </Link>
+    <Link href="/about">
+      <a> About </a>
+    </Link>
+    <Link href="/blogs">
+      <a> Blog </a>
+    </Link>
+    <Link href="/cv">
+      <a> CV </a>
+    </Link>
+    <Link href="/portfolios">
+      <a> Portfolios </a>
+    </Link>
   </div>
 )
 
