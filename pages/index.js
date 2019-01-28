a15ceaf7b337212f690b39382dd5e86cedaf016e
Jayce77 <xjaycex@hotmail.com>
2019-01-27 21:12:10 -0500

    implemented getInitialProps with axios GET

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
-import Header from '../components/shared/header'
+import React from 'react'
 import BaseLayout from '../components/layouts/base-layout'
+import axios from 'axios'
 
-const Index = () => (
-  <div>
-    <BaseLayout>
-      <p>Hello Next.js</p>
-    </BaseLayout>
-  </div>
-)
+class Index extends React.Component {
+
+  static async getInitialProps() {
+    let userData = {}
+    
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      userData = response.data
+    } catch(err) {
+      console.log(err)
+    }
+    return { userData }
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const intialData = this.props.userData
+    console.log(intialData)
+
+    return(
+      <div>
+        <BaseLayout>
+          <p>Hello Next.js</p>
+        </BaseLayout>
+      </div>
+    )
+  } 
+    
+}
 
 export default Index
