b4f2d6c2f164fd18f5c46fe7481e4d9c8d791e16
Jayce77 <xjaycex@hotmail.com>
2019-02-18 20:53:04 -0500

    Added reactstrap for bootstrap components

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
+const withCss = require('@zeit/next-css')
+
 module.exports = withSass()
+module.exports = withCss()
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,25 @@
 import React from 'react'
 import BaseLayout from '../components/layouts/base-layout'
-import axios from 'axios'
+import { Button, Container } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Index extends React.Component {
-
-  static async getInitialProps() {
-    let userData = {}
-    
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-      userData = response.data
-    } catch(err) {
-      console.log(err)
-    }
-    return { userData }
-  }
 
   constructor(props) {
     super(props);
   }
 
   render() {
-    const intialData = this.props.userData
-    console.log(intialData)
 
     return(
       <div>
         <BaseLayout>
-          <p>Hello Next.js</p>
+          <Container>
+            <Button 
+              color="danger">
+              Danger!
+            </Button>
+          </Container>
         </BaseLayout>
       </div>
     )
