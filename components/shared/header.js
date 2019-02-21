53271bdd7577f40b668f58698876be732ab9569e
Jayce77 <xjaycex@hotmail.com>
2019-02-20 22:09:37 -0500

    Integrated Navbar into Header

--- components/shared/header.js
+++ components/shared/header.js
@@ -1,26 +1,66 @@
 import React from 'react'
 import Link from 'next/link'
+import {
+  Navbar,
+  Nav,
+  NavbarBrand,
+  NavItem,
+  NavbarToggler,
+  Collapse
+} from 'reactstrap'
+
+const BsNavLink = ({ route, title }) => (
+  <Link href={route}>
+    <a className="nav-link"> {title} </a>
+  </Link>
+)
 
 class Header extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
 
   render() {
     return (
       <div>
-        <Link href="/">
-          <a> Home </a>
-        </Link>
-        <Link href="/about">
-          <a> About </a>
-        </Link>
-        <Link href="/portfolios">
-          <a> Portfolios </a>
-        </Link>
-        <Link href="/blogs">
-          <a> Blog </a>
-        </Link>
-        <Link href="/cv">
-          <a> CV </a>
-        </Link>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Jayce Crowther</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <BsNavLink route={"/"} title={'Home'} />
+            </NavItem>
+            <NavItem>
+              <BsNavLink route={"/about"} title={'About'} />
+            </NavItem>
+            <NavItem>
+              <BsNavLink route={"/portfolios"} title={'Portfolios'} />
+            </NavItem>
+            <NavItem>
+                <BsNavLink route={"/blogs"} title={'Blog'} />
+            </NavItem>
+            <NavItem>
+              <BsNavLink route={"/cv"} title={'CV'} />
+            </NavItem>
+          </Nav>
+        </Collapse>
+        
+        </Navbar>
+        
+        
+        
         <style jsx>
           {`
             a {
