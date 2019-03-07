2cefb5a94ddcb92a55fcc04e8220f383975c0857
Jayce77 <xjaycex@hotmail.com>
2019-03-06 21:43:20 -0500

    Added auth0 login and logout methods and cookie storage

--- services/auth0.js
+++ services/auth0.js
@@ -1,5 +1,5 @@
-import auth0 from 'auth0-js';
-import { resolve } from 'url';
+import auth0 from 'auth0-js'
+import Cookies from 'js-cookie'
 
 class Auth0 {
   
@@ -14,7 +14,9 @@ class Auth0 {
     });
 
     this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
     this.handleAuthentication = this.handleAuthentication.bind(this)
+    this.isAuthenticated = this.isAuthenticated.bind(this)
   }
 
   login() {
@@ -37,8 +39,33 @@ class Auth0 {
 
   }
 
-  setSession() {
-    // Save tokens
+  setSession(authResult) {
+    debugger
+    // Set the time that the access token will expire at
+    const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    // this.accessToken = authResult.accessToken;
+
+    Cookies.set('user', authResult.idTokenPayload)
+    Cookies.set('jwt', authResult.idToken)
+    Cookies.set('expiresAt', expiresAt)
+  }
+
+  logout() {
+    Cookies.remove('user')
+    Cookies.remove('jwt')
+    Cookies.remove('expiresAt')
+
+    this.auth0.logout({
+      returnTo: '',
+      clientID: 'NI7a5qlNmvsANAdXW3GeAaq1kpCdK4aA'
+    })
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = Cookies.getJSON('expiresAt')
+    return new Date().getTime() < expiresAt
   }
 }
 
